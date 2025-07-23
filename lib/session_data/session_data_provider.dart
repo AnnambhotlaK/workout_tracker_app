0d672cf14ece8d8dabf980fa85afc5e1ce085b54
Kishore Annambhotla <k4annam@gmail.com>
2025-07-23 14:06:01 -0400

    Implemented session methods for firestore_service
    Next things to do: Finish session backend, connect backend services with user id auth, and test thoroughly!!! Then UI. May take 1+ week

---
+++ lib/session_data/session_data_provider.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+import '../models/exercise.dart';
+import '../services/firestore_service.dart';
+import '../models/workout.dart';
+import '../models/set.dart';
